b63f401bdeba6850eeb6a612119d4688d0307775
EDGW <edgwunderline@outlook.com>
2026-03-13 17:07:23 +0800

    Updated the build scripts to use LLVM tools for binutils.

--- scripts/xtask/src/config/build.rs
+++ scripts/xtask/src/config/build.rs
@@ -4,30 +4,14 @@ use crate::config::platform::*;
 impl TargetTriple {
     pub fn objdump(&self) -> &'static str {
         match self {
-            Self::RiscV64UnknownAnemoneElf => {
-                if cfg!(target_os = "linux") {
-                    "riscv64-unknown-elf-objdump"
-                } else if cfg!(target_os = "macos") {
-                    "riscv64-elf-objdump"
-                } else {
-                    unimplemented!("Objdump for target triple {:?} is not implemented on this platform", self)
-                }
-            },
+            Self::RiscV64UnknownAnemoneElf => "llvm-objdump",
             _ => unimplemented!("Objdump for target triple {:?} is not implemented", self),
         }
     }
 
     pub fn objcopy(&self) -> &'static str {
         match self {
-            Self::RiscV64UnknownAnemoneElf => {
-                if cfg!(target_os = "linux") {
-                    "riscv64-unknown-elf-objcopy"
-                } else if cfg!(target_os = "macos") {
-                    "riscv64-elf-objcopy"
-                } else {
-                    unimplemented!("Objcopy for target triple {:?} is not implemented on this platform", self)
-                }
-            },
+            Self::RiscV64UnknownAnemoneElf => "llvm-objcopy",
             _ => unimplemented!("Objcopy for target triple {:?} is not implemented", self),
         }
     }
