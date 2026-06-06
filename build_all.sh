747072bd3099bc9bf528a7037c1ce3b05be8d692
EDGW <edgwunderline@outlook.com>
2026-06-06 20:13:16 +0800

    fixed some problem

--- build_all.sh
+++ build_all.sh
@@ -4,11 +4,15 @@ export RUSTUP_DIST_SERVER=https://rsproxy.cn
 
 set -euo pipefail
 
+apt install libguestfs-tools
 cargo install just
 cp conf/.benchconf kconfig
 
 # RV
 # for build
+rm -rf rv-base
+mkdir -p rv-base/dev
+mkdir -p rv-base/mnt
 dd if=/dev/zero of=sdcard-rv.img bs=1M count=1
 just conf switch pre-test-rv64
 just rootfs mkfs --config conf/rootfs/test-rv
@@ -16,7 +20,10 @@ cp build/rootfs/test-rv/rootfs.img disk.img
 just build
 cp build/anemone.elf kernel-rv
 
-# LA currently not supported
+# LA
+# rm -rf la-base
+# mkdir -p la-base/dev
+# mkdir -p la-base/mnt
 # just conf switch pre-test-la64
 # just rootfs mkfs --config conf/rootfs/test-la
 # cp build/rootfs/test-la/rootfs.img disk-la.img
