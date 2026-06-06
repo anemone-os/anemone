9f869f2dbf611eb9bd30014880701802b29f74bd
EDGW <edgwunderline@outlook.com>
2026-06-06 15:56:36 +0800

    fixed scripts

--- build_all.sh
+++ build_all.sh
@@ -6,9 +6,19 @@ set -euo pipefail
 
 cargo install just
 cp conf/.benchconf kconfig
-just conf switch rv
+
+# RV
+# for build
+dd if=/dev/zero of=sdcard-rv.img bs=1M count=1
+just conf switch pre-test-rv64
+just rootfs mkfs --config conf/rootfs/test-rv
+cp build/rootfs/test-rv/rootfs.img disk.img
 just build
 cp build/anemone.elf kernel-rv
-just conf switch la
-just build
-cp build/anemone.elf kernel-la
+
+# LA currently not supported
+# just conf switch pre-test-la64
+# just rootfs mkfs --config conf/rootfs/test-la
+# cp build/rootfs/test-la/rootfs.img disk-la.img
+# just build
+# cp build/anemone.elf kernel-la
