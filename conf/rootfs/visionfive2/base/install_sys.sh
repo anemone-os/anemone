8bd21762bb8eb9792236701a1ca75a959addcbab
EDGW <edgwunderline@outlook.com>
2026-08-01 20:06:21 +0800

    fix: changed rootfs install scripts

--- conf/rootfs/visionfive2/base/install_sys.sh
+++ conf/rootfs/visionfive2/base/install_sys.sh
@@ -9,15 +9,19 @@ BUSYBOX=/home/bin/busybox
 ROOT=${1:-/}
 ROOT=${ROOT%/}
 INSTALL_MARKER="$ROOT/etc/.anemone-installed"
+REINSTALL_MARKER=/home/.anemone_need_reinstall
 INSTALL_VERSION=2
 
-if [ -f "$INSTALL_MARKER" ] \
+if [ ! -f "$REINSTALL_MARKER" ] \
+    && [ -f "$INSTALL_MARKER" ] \
     && [ "$("$BUSYBOX" cat "$INSTALL_MARKER")" = "$INSTALL_VERSION" ]; then
     echo "Anemone toolchain is already installed."
     exit 0
 fi
 
 "$BUSYBOX" mkdir -p "$ROOT/etc" "$ROOT/root" "$ROOT/usr/bin" "$ROOT/usr/sbin"
+"$BUSYBOX" ln -sfn /usr/bin "$ROOT/bin"
+"$BUSYBOX" ln -sfn /usr/sbin "$ROOT/sbin"
 
 for applet in sh ls init; do
     echo "Installing $applet..."
@@ -30,5 +34,6 @@ done
 "$BUSYBOX" ln -s /usr/bin/init "$ROOT/usr/sbin/init"
 "$BUSYBOX" ln -sf /home/sbin/shutdown "$ROOT/usr/bin/poweroff"
 echo "$INSTALL_VERSION" > "$INSTALL_MARKER"
+"$BUSYBOX" rm -f "$REINSTALL_MARKER"
 
 echo "Anemone toolchain is ready."
